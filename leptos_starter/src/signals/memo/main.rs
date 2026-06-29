2e647f20dfba122e732a6d4a2e9db1213bcfb193
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-06-29 07:53:52 +0200

    trying to figure out what the next thing i wanna do is aswell as fixing the old code i was working on. it was getting unecessarily messy, but now I'm running into errors I don't really understand.

--- leptos_starter/src/signals/memo/main.rs
+++ leptos_starter/src/signals/memo/main.rs
@@ -1,6 +1,6 @@
 use leptos::html;
 use leptos::prelude::*;
-use leptos::tachys::reactive_graph::bind::GetValue;
+//use leptos::tachys::reactive_graph::bind::GetValue;
 use leptos_router::components::A; // for making <A> work
 use leptos_router::components::{Route, Router, Routes};
 use leptos_router::path; // for the path!() macro
@@ -31,7 +31,7 @@ fn DefaultPage() -> impl IntoView {
     let (data, data_set) = signal("Example Data".to_string());
 
     let input_ref: NodeRef<html::Input> = NodeRef::new();
-    let display_ref = create_node_ref::<html::Div>();
+    let display_ref: NodeRef<html::Div> = NodeRef::new();
     view! {
         <table>
             <tr>
@@ -44,17 +44,19 @@ fn DefaultPage() -> impl IntoView {
                         <hr/>
 
                         <div>
-                            <button on:click=move |_| {
+                            /*<button on:click=move |_| {
                                 let value = input_ref.get().unwrap().value();
-                                display_ref.get().unwrap_or_default().set_inner_html(&value);
+                                let Some(html_div) = display_ref.get() {
+                                    html_div.set_inner_html(&value);
+                                };
+                                //display_ref.get().unwrap_or_default().set_inner_html(&value);
                             }
-                            >"Click button to store input"</button>
+                            >"Click button to store input"</button>*/
                             <hr/>
                         </div>
                         <div>
                             <button
                                 on:click=move |_|  {
-                                    unwrap_or_go_to_bed(&data_set);
                                     //data_set.set(input_ref.get().unwrap_or("".to_string()).value());
                                 }
                             >"Click button to show stored data"</button>
@@ -66,20 +68,16 @@ fn DefaultPage() -> impl IntoView {
                 </td>
                 <td>
                     "Data stored: " {
-                        //let value =
-                        display_ref.get().unwrap_or_default().inner_html();
-                        /*if let Some(x) = value {
-                            return x;
+                        /*let html_div = ;*/
+                        /*if let Some(html_div) = display_ref.get() {
+                            return html_div.inner_html();
+                        } else {
+                            return "".to_string();
                         }*/
+                        //"test".to_string()
                     }
                 </td>
             </tr>
         </table>
     }
-}
-
-fn unwrap_or_go_to_bed<T>(value: &T)
-where T: NodeRefAttribute
--> T {
-    //data_set.set(input_ref.get().unwrap_or("".to_string()).value());
 }

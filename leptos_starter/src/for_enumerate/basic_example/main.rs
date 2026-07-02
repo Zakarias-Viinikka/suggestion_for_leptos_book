f21e4db49167d386a5d7f1406e9b53034b98f846
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-07-02 06:45:55 +0200

    just making code a little nicer

--- leptos_starter/src/for_enumerate/basic_example/main.rs
+++ leptos_starter/src/for_enumerate/basic_example/main.rs
@@ -1,7 +1,6 @@
 use leptos::prelude::*;
 use rand::seq::IndexedRandom; //rng stuff
-/*
-use rand::prelude::IndexedRandom; //for getting a random item from array easily.*/
+
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(App);
@@ -16,24 +15,28 @@ struct Fruit {
 #[component]
 fn App() -> impl IntoView {
     let list: [Fruit; 1000] = std::array::from_fn(|i| Fruit {
-        name: RwSignal::new("Placeholder".to_string()),
+        name: RwSignal::new("Placeholder".to_string()), //need to use a signal
         id: i,
     });
     let (list, _) = signal(list);
     view! {
         <div class="container">
+        /*important */
+        // fruit.id is how leptos tracks stuff properly, makes it 'reactive' and whatnot. without it stuff just breaks.
+        /*important */
             <ForEnumerate
                 each=move || list.get() //signal.get() normal stuff
-                key=|fruit| fruit.id // fruit is the equivalent of 'for fruit in list' // fruit.id is how leptos tracks stuff properly. without it stuff just breaks.
+
+                key=|fruit| fruit.id // fruit is the equivalent of 'for fruit in list'
                 let(index, fruit) // index is fruit.id and fruit is fruit
             >
                 //view! { // with the let syntax view! isn't needed
-                    <button on:click=move |_| {
-                        fruit.name.set(get_random_fruit());
-                    }>
-                        {list.get()[index.get()].name.clone()}
-                    </button>
-                    //}
+                <button on:click=move |_| {
+                    fruit.name.set(get_random_fruit());
+                }>
+                    {list.get()[index.get()].name.clone()}
+                </button>
+                //}
             </ForEnumerate>
         </div>
     }

6c768a6effccce985f341a82c7642f9b54ca6cd9
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-07-07 10:27:24 +0200

    figured out how i can do js tests potentially

---
+++ leptos_starter/src/animation/simple_example/main.rs
@@ -0,0 +1,11 @@
+/*
+* came up with a way to do js tests that is relatively painfree and auotmated and works well with trunk:
+*
+* is there a way i can make the browser automatically download/edit a file in like /downloads.
+
+that way i could write a program that just checks that file and then gives my
+computer a popup that is like "hey ur file suggests something is wrong
+with your js tests" and then i could just have a component this is like
+<Tests /> and everytime my trunk reloads my tests would get ran
+and the file would get updated, but only when i worked on the page.
+*/

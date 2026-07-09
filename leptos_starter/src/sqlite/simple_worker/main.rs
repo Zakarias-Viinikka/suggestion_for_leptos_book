a31d7181480106518963458e3faf57d50cf404da
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-07-09 11:20:46 +0200

    think i'm halfway there for getting a simple worker to work assuming the official trunk repo example i found works. left instructions in instructions.txt

--- leptos_starter/src/sqlite/simple_worker/main.rs
+++ leptos_starter/src/sqlite/simple_worker/main.rs
@@ -1,5 +1,3 @@
-//use crate::sqlite::read_from::{black_magic, db_table::Table};
-use leptos_workers::worker;
 use serde::{Deserialize, Serialize};
 
 use leptos::logging::log;
@@ -8,6 +6,11 @@ use leptos::prelude::*;
 //cargo add gloo-timers
 use gloo_timers::future::sleep;
 
+//for the actual worker stuff
+// cargo add gloo-worker --features futures
+use gloo_worker::Spawnable;
+use gloo_worker::oneshot::oneshot;
+
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(App);
@@ -15,12 +18,11 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    /* the reason this doesn't work is because, as of writin leptos_workers only works for leptos versions up to 0.8 */
-    //let db_response = LocalResource::new(async move || worker_test(MyRequest).await);
-    //
-    // found a resource i can check out so it's easier for me to know where to start next time
     // https://docs.rs/gloo-worker/latest/gloo_worker/#worker-trait
-    let tmp = LocalResource::new(async move || simple_ping("".to_string()).await);
+    let response = LocalResource::new(move || async move {
+        let mut caller = do_work_on_different_thread::spawner().spawn("./worker.js");
+        caller.run("input".to_string()).await
+    });
     view! {
         <div class="container">
             /*<p>
@@ -35,10 +37,8 @@ fn App() -> impl IntoView {
     }
 }
 
-//cargo add leptos_workers
-// cargo add serde --features derive
-#[worker(MyFutureWorker)]
-async fn worker_test(my_request: MyRequest) -> MyResponse {
+#[oneshot]
+async fn do_work_on_different_thread(my_request: String) -> MyResponse {
     sleep(std::time::Duration::from_secs(2)).await;
     MyResponse::Success
 }
@@ -51,8 +51,3 @@ pub enum MyResponse {
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct MyRequest;
-
-#[worker]
-async fn simple_ping(a: String) -> String {
-    "pong".to_string()
-}
